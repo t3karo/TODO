862285e02a60a6c60e680ac810087f1724022c46
Roshan Kandel <t3karo02@students.oamk.fi>
2024-03-20 11:22:41 +0200

    part2 and 3 completed

---
+++ server/index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const cors = require('cors')
+const { Pool } = require('pg')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const port = 3001
+
+app.get('/',(req,res) => {
+    const pool = openDb()
+
+    pool.query('select * from task', (error, result) => {
+        if (error) {
+            res.status(500).json({error: error.message})
+        }
+        res.status(200).json(result.rows)
+    })
+})
+
+const openDb = () => {
+    const pool= new Pool ({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'todo',
+        password: 'sql123',
+        port: 5432
+    });
+    
+    return pool;
+}
+app.post('/new', (req, res) => {
+    const pool = openDb();
+
+    pool.query("INSERT INTO task (description) VALUES ($1) returing *",
+     [req.body.description],
+     (error, result) => {
+        if(error) {
+            res.status(500).json({error: error.message})
+        } else {
+            res.status(201).json({id : result.rows[0].id})
+        }
+    })
+})
+
+app.listen(port)
